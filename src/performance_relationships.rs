ab68fce432af35b0f63e0aafcf2e3d6e0efc39ae
Robert Greener <me@r0bert.dev>
2022-09-27 17:42:20 +0100

    Test and document performance_relationships
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/performance_relationships.rs
+++ src/performance_relationships.rs
@@ -4,8 +4,19 @@ use uuid::Uuid;
 
 use crate::json::PerformanceRelationshipSpec;
 
+/// The value is how much someone holding the [Belief] would like to perform
+/// the [Behaviour].
 pub type PerformanceRelationships = HashMap<(*const dyn Belief, *const dyn Behaviour), f64>;
 
+/// Convert [PerformanceRelationshipSpec]s to [PerformanceRelationships].
+///
+/// # Arguments
+/// - `prss`: The [PerformanceRelationshipSpec].
+/// - `belief`: The [Belief]s mapped from their [Uuid]s.
+/// - `behaviour`: The [Behaviour]s mapped from their [Uuid]s.
+///
+/// # Returns
+/// The [PerformanceRelationships].
 pub fn vec_prs_to_performance_relationships(
     prss: &[PerformanceRelationshipSpec],
     beliefs: &HashMap<Uuid, *const dyn Belief>,
@@ -22,4 +33,37 @@ pub fn vec_prs_to_performance_relationships(
             )
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use belief_spread::{BasicBehaviour, BasicBelief, Behaviour, Belief, UUIDd};
+    use uuid::Uuid;
+
+    use crate::json::PerformanceRelationshipSpec;
+
+    use super::vec_prs_to_performance_relationships;
+
+    #[test]
+    fn test_vec_prs_to_performance_relationships_works() {
+        let mut prss: Vec<PerformanceRelationshipSpec> = Vec::new();
+        let belief = BasicBelief::new("b1".to_string());
+        let behaviour = BasicBehaviour::new("b1".to_string());
+        prss.push(PerformanceRelationshipSpec {
+            behaviour_uuid: behaviour.uuid().clone(),
+            belief_uuid: belief.uuid().clone(),
+            value: 0.2,
+        });
+        let mut beliefs: HashMap<Uuid, *const dyn Belief> = HashMap::new();
+        beliefs.insert(belief.uuid().clone(), &belief);
+
+        let mut behaviours: HashMap<Uuid, *const dyn Behaviour> = HashMap::new();
+        behaviours.insert(behaviour.uuid().clone(), &behaviour);
+
+        let result = vec_prs_to_performance_relationships(&prss, &beliefs, &behaviours);
+        assert_eq!(result.len(), 1);
+        assert_eq!(*result.get(&(&belief, &behaviour)).unwrap(), 0.2)
+    }
 }
