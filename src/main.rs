7786eb9872ccf735fda0666c770b267a701f9513
Robert Greener <me@r0bert.dev>
2022-09-27 10:09:56 +0100

    Store config in struct on heap
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod json;
 
-use std::{fs::File, io};
+use std::{
+    fs::File,
+    io,
+    ptr::{null, null_mut, slice_from_raw_parts, slice_from_raw_parts_mut},
+};
 
 use anyhow::{Context, Result};
-use belief_spread::{BasicBehaviour, SimTime};
+use belief_spread::{Agent, BasicBehaviour, Behaviour, Belief, SimTime};
 use clap::Parser;
 use json::BehaviourSpec;
 
@@ -38,10 +42,58 @@ struct Cli {
     behaviours_file: std::path::PathBuf,
 }
 
+/// The configuration of the model.
+struct Configuration {
+    /// The [Behaviour]s in the model.
+    behaviours: *const [*const dyn Behaviour],
+
+    /// The [Belief]s in the model.
+    beliefs: *const [*const dyn Belief],
+
+    /// The [Agent]s in the model.
+    agents: *const [*const dyn Agent],
+
+    /// The mutable [Behaviour]s in the model.
+    behaviours_mut: *mut [*mut dyn Behaviour],
+
+    /// The mutable [Belief]s in the model.
+    beliefs_mut: *mut [*mut dyn Belief],
+
+    /// The mutable [Agent]s in the model.
+    agents_mut: *mut [*mut dyn Agent],
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let _behaviours = read_behaviours_json(&args.behaviours_file)?;
+    let mut config: Box<Configuration> = Box::new(Configuration {
+        behaviours: slice_from_raw_parts(null(), 0),
+        beliefs: slice_from_raw_parts(null(), 0),
+        agents: slice_from_raw_parts(null(), 0),
+        behaviours_mut: slice_from_raw_parts_mut(null_mut(), 0),
+        beliefs_mut: slice_from_raw_parts_mut(null_mut(), 0),
+        agents_mut: slice_from_raw_parts_mut(null_mut(), 0),
+    });
+
+    let mut behaviours = read_behaviours_json(&args.behaviours_file)?;
+
+    let mut behaviours_ptrs_mut: Vec<*mut dyn Behaviour> = behaviours
+        .iter_mut()
+        .map(|b| b as *mut dyn Behaviour)
+        .collect();
+
+    let behaviours_ptrs_mut_slice: &mut [*mut dyn Behaviour] = &mut behaviours_ptrs_mut;
+
+    config.behaviours_mut = behaviours_ptrs_mut_slice;
+
+    let behaviours_ptrs: Vec<*const dyn Behaviour> = behaviours
+        .iter()
+        .map(|b| b as *const dyn Behaviour)
+        .collect();
+
+    let behaviours_ptrs_slice: &[*const dyn Behaviour] = &behaviours_ptrs;
+
+    config.behaviours = behaviours_ptrs_slice;
 
     Ok(())
 }
