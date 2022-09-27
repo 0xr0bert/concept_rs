b7851d15e06ce16c955538998688c7cae516e8b2
Robert Greener <me@r0bert.dev>
2022-09-27 08:38:05 +0100

    Add CLI stub with start_time
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use belief_spread::SimTime;
+use clap::Parser;
+
+/// The arguments of the command-line interface
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// The start time of the simulation
+    #[clap(short = 's', long = "start", value_parser, default_value_t = 1)]
+    start_time: SimTime,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Cli::parse();
 }
